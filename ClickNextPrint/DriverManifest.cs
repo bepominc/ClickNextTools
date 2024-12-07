dc2c2588126acbfc01d029a6b8f3b331bb6096d5
Benjamin H. <ben@metaresolve.com>
2024-12-06 16:51:21 -0800

    Add file select, start work on INF parsing

---
+++ ClickNextPrint/DriverManifest.cs
@@ -0,0 +1,43 @@
+﻿using InfHelper;
+using InfHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickNextPrint
+{
+    internal class DriverManifest
+    {
+        private string FileContent;
+
+        public DriverManifest(string content)
+        {
+            this.FileContent = content;
+        }
+
+        public String[] GetDriverList()
+        {
+            InfUtil infUtil = new InfUtil();
+            InfData infData = infUtil.Parse(this.FileContent);
+
+            Key manufacturerData = infData["Manufacturer"].Keys[0];
+
+            string[] driverCategories = [];
+            for (int i = 0; i < manufacturerData.KeyValues.Count; i++)
+            {
+                if (i == 0)
+                {
+                    driverCategories.Append(manufacturerData.KeyValues[0].Value);
+                    continue;
+                }
+                driverCategories.Append(manufacturerData.KeyValues[0].Value + "." + manufacturerData.KeyValues[i].Value);
+            }
+
+            // TODO: Iterate over driver categories and build a list of drivers from the keys in each category.
+            string[] drivers = [];
+            return drivers;
+        }
+    }
+}
--- ClickNextPrint/MainWindow.xaml.cs
+++ ClickNextPrint/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,19 +24,57 @@ namespace ClickNextPrint
             InitializeComponent();
         }
 
-        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        private void SelectDriverButton_Click(object sender, RoutedEventArgs e)
         {
+            FileDialog dialog = new OpenFileDialog();
+            dialog.DefaultExt = ".inf";
+            dialog.Filter = "Driver manifests (*.inf)|*.inf";
 
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                try
+                {
+                    DriverPathBox.Text = dialog.FileName;
+                    DriverPathBox.ToolTip = dialog.FileName;
+
+                    string manifestText = File.ReadAllText(DriverPathBox.Text);
+                    DriverManifest driverManifest = new DriverManifest(manifestText);
+
+                    PrinterListBox.Items.Clear();
+                    foreach (string driver in driverManifest.GetDriverList())
+                    {
+                        PrinterListBox.Items.Add(driver);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error parsing manifest", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    DriverPathBox.Text = null;
+                    DriverPathBox.ToolTip = null;
+                }
+            }
         }
 
-        private void PrintSidingComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void DuplexingComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            Debug.WriteLine(DuplexingComboBox.SelectedValue);
         }
 
         private void BuildIntuneButton_Click(object sender, RoutedEventArgs e)
         {
+            FileDialog dialog = new SaveFileDialog();
+            dialog.FileName = PrinterNameBox.Text;
+            dialog.DefaultExt = ".intunewin";
+            dialog.Filter = "Intune win32 app (*.intunewin)|*.intunewin";
 
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                // TODO: Kick off printer bundle compilation.
+            }
         }
     }
 }
